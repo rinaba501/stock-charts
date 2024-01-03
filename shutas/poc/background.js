4c611ad7350b886571ce309563be55a59f9f6278
shutas <shutas@umich.edu>
2024-01-04 00:07:17 +0900

    ADD: Display box for context menu

--- shutas/poc/background.js
+++ shutas/poc/background.js
@@ -11,15 +11,25 @@ chrome.runtime.onInstalled.addListener(function (details) {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case "displayStockInformation":
+      let word = info.selectionText;  // 選択文字列を取得する
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: displayStockInfo,
       });
-      let word = info.selectionText;  // 選択文字列を取得する
       break;
   }
 });
 
 function displayStockInfo() {
-  window.alert('testing🎉');
+  //window.alert('testing🎉');
+  var div = document.createElement("div");
+  div.style.position = "fixed";
+  div.style.top = "5px";
+  div.style.left = "5px";
+  div.style.width = "200px";
+  div.style.height = "200px";
+  div.style.backgroundColor = "black";
+  div.style.color = "white";
+  div.innerHTML = "Stock info goes here";
+  document.body.appendChild(div);
 }
